3d71eebfeee63941234c56a9a8f5ba38928afade
bpeelman <brody.peelman@gmail.com>
2024-04-21 04:01:44 -0400

    awojgnoggqqowoiwg3oig[goi3

--- hackathon/src/components/ResponsePage.vue
+++ hackathon/src/components/ResponsePage.vue
@@ -1,6 +1,8 @@
 <template>
         <h1>Here's your personalized therapy!!!!</h1>
-        <p :style="{color: 'red'}">{{ $route.query.message }}</p>
+        <p :style="{backgroundColor: 'white', color: 'black', lineHeight: 1.75, fontSize: 'calc(0.5em + 1vw)', border: '2px solid black', borderRadius: '50px', padding: '30px', textAlign: 'justify', height: '40%'}">
+        {{ $route.query.message }}
+        </p>
 </template>
   <script>
   export default {
